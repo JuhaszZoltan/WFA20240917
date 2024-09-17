abe8eb11cbd29e20ebf6f484472ad5f6a5a56760
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-17 13:18:06 +0200

    updt

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -30,6 +30,8 @@
         {
             pnlMain = new Panel();
             btnClickMe = new Button();
+            lblTime = new Label();
+            lblCatches = new Label();
             pnlMain.SuspendLayout();
             SuspendLayout();
             // 
@@ -39,9 +41,9 @@
             pnlMain.AutoSize = true;
             pnlMain.BackColor = Color.Gainsboro;
             pnlMain.Controls.Add(btnClickMe);
-            pnlMain.Location = new Point(12, 12);
+            pnlMain.Location = new Point(12, 60);
             pnlMain.Name = "pnlMain";
-            pnlMain.Size = new Size(860, 837);
+            pnlMain.Size = new Size(860, 789);
             pnlMain.TabIndex = 0;
             // 
             // btnClickMe
@@ -49,23 +51,46 @@
             btnClickMe.BackColor = Color.IndianRed;
             btnClickMe.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             btnClickMe.ForeColor = Color.White;
-            btnClickMe.Location = new Point(0, 0);
+            btnClickMe.Location = new Point(3, 3);
             btnClickMe.Name = "btnClickMe";
             btnClickMe.Size = new Size(100, 100);
             btnClickMe.TabIndex = 0;
             btnClickMe.Text = "Click me!";
             btnClickMe.UseVisualStyleBackColor = false;
             // 
+            // lblTime
+            // 
+            lblTime.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            lblTime.Font = new Font("Consolas", 20F, FontStyle.Bold);
+            lblTime.Location = new Point(709, 9);
+            lblTime.Name = "lblTime";
+            lblTime.Size = new Size(163, 48);
+            lblTime.TabIndex = 1;
+            lblTime.Text = "00:00:00";
+            lblTime.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // lblCatches
+            // 
+            lblCatches.Font = new Font("Consolas", 20F, FontStyle.Bold);
+            lblCatches.Location = new Point(12, 9);
+            lblCatches.Name = "lblCatches";
+            lblCatches.Size = new Size(209, 48);
+            lblCatches.TabIndex = 1;
+            lblCatches.Text = "catches: 00";
+            lblCatches.TextAlign = ContentAlignment.MiddleLeft;
+            // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(884, 861);
+            Controls.Add(lblCatches);
+            Controls.Add(lblTime);
             Controls.Add(pnlMain);
             Font = new Font("Segoe UI", 12F);
             Margin = new Padding(4);
             Name = "FrmMain";
-            Text = "Form1";
+            Text = "Catch me!";
             pnlMain.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
@@ -75,5 +100,7 @@
 
         private Panel pnlMain;
         private Button btnClickMe;
+        private Label lblTime;
+        private Label lblCatches;
     }
 }
--- FrmMain.cs
+++ FrmMain.cs
@@ -1,26 +1,65 @@
+using System.Diagnostics;
+
 namespace WFA20240917
 {
     public partial class FrmMain : Form
     {
-        static Random rnd = new();
+        private static Random rnd = new();
+        private Stopwatch sw = new();
+        private System.Windows.Forms.Timer timer = new();
+        private int catches = 0;
+        private TimeSpan lastColorChange = TimeSpan.Zero;
         public FrmMain()
         {
             InitializeComponent();
+            timer.Interval = 100;
+            timer.Start();
             btnClickMe.Click += BtnClickMeClick;
             btnClickMe.MouseEnter += BtnClickMeMouseEnter;
+            pnlMain.MouseMove += PnlMain_MouseMove;
+            timer.Tick += TmrTick;
+            this.MinimumSize = new(
+                width: 500,
+                height: 500);
+        }
 
-            //ha a panelen mozog a kurzor -> váltson random színre
-            //számold egy változóbán, hogy hányszor sikerült elkapni a gombot
-            //5 catch után írja ki az eltelt idõt az MsgBox-ban!
+        private void TmrTick(object? sender, EventArgs e)
+        {
+            lblTime.Text = $"{sw.Elapsed.Minutes:00}" +
+                $":{sw.Elapsed.Seconds:00}" +
+                $":{sw.Elapsed.Milliseconds:000}";
+        }
+
+        private void PnlMain_MouseMove(object? sender, MouseEventArgs e)
+        {
+            if (lastColorChange.Add(new(00, 00, 00, 00, 200)) < sw.Elapsed)
+            {
+                btnClickMe.BackColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+                lastColorChange = sw.Elapsed;
+            }
         }
 
         private void BtnClickMeMouseEnter(object? sender, EventArgs e)
         {
             btnClickMe.Location = new(
-                x: rnd.Next(pnlMain.Height - btnClickMe.Height),
-                y: rnd.Next(pnlMain.Width - btnClickMe.Width));
+                x: rnd.Next(pnlMain.Width - btnClickMe.Width),
+                y: rnd.Next(pnlMain.Height - btnClickMe.Height));
+            if (!sw.IsRunning) sw.Start();
         }
-        private void BtnClickMeClick(object? sender, EventArgs e) => 
-            MessageBox.Show("You WIN!");
+        private void BtnClickMeClick(object? sender, EventArgs e)
+        {
+            catches++;
+            lblCatches.Text = $"catches: {catches:00}";
+            BtnClickMeMouseEnter(null, null!);
+            if(catches >= 5)
+            {
+                MessageBox.Show(
+                    caption: "YOU WIN!",
+                    text: $"time: {sw.Elapsed.TotalSeconds} sec.\n" +
+                    $"pnl size: [{pnlMain.Width} x {pnlMain.Height}]");
+                Application.Exit();
+            }
+        }
+
     }
 }
